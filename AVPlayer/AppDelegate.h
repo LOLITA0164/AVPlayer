8939bdabcf0ca76d41d637e670e0981b6812666e
LOLITA <476512340@qq.com>
2017-11-28 13:45:48 +0800

    Initial Commit

---
+++ AVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayer
+//
+//  Created by LOLITA on 2017/11/28.
+//  Copyright © 2017年 LOLITA0164. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVPlayer
+//
+//  Created by LOLITA on 2017/11/28.
+//  Copyright © 2017年 LOLITA0164. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
